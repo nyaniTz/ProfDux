3135f91b91d8de2cde48d1d8d47f8402aeb168e5
Ibrahim Ame <ai.iot@neu.edu.tr>
2024-02-10 15:35:54 +0200

    Course Addinng and Removing Inputs

--- templates/Course.js
+++ templates/Course.js
@@ -1,22 +1,37 @@
 class Course {
 
     status =  "unchaged"
+    id
     title
     courseCode
     lectures
-    courseObject
+
+    lectureIndex = 0
+    
+    lectureUpdates = {}
+    subtopicUpdates = {}
+
+    newLectures = {}
+    newSubtopics = {}
+    newResources = {}
+
+    deleteLectures = {}
+    deleteSubtopic = {}
+    deleteResource = {}
 
     constructor(courseObject){
         let {
             title,
             courseCode,
             lectures,
+            id,
         } = courseObject
 
         this.title = title;
         this.courseCode = courseCode;
         this.lectures = lectures;
         this.courseObject = courseObject;
+        this.id = id
     }
 
     setTitle(title, _this){
@@ -41,50 +56,64 @@ class Course {
 
     addLectureElement(){
 
+        let lectureID = uniqueID(1);
+
+        let courseGridContainer = document.querySelector("#course-grid-container");
+
+        let lectureSection = document.createElement("div");
+        lectureSection.className = "lecture-section";
+
+        let itemizationElement = document.createElement("div");
+        itemizationElement.className = "itemization";
+        itemizationElement.textContent = `${++this.lectureIndex}.`;
+
+        let lectureInnerContainer = document.createElement("div");
+        lectureInnerContainer.className = "lecture-inner-container";
+
+        let lectureInputElement = this.createInputElement(lectureID, "", this.newLectureTitle, "lecture");
+
+        lectureInnerContainer.appendChild(lectureInputElement);
+
+        // lectureInnerContainer.appendChild(subtopicsContainer);
+        lectureSection.appendChild(itemizationElement);
+        lectureSection.appendChild(lectureInnerContainer);
+        courseGridContainer.appendChild(lectureSection);
+
+        console.log("Done adding lecture element")
     }
-
-    deleteLectureElement(){
-
-    }
-
     updateLectureTitle(id, title, _this){
-
-        _this.lectures.forEach( (lecture, index) => {
-            if(lecture.id == id){
-                _this.lectures[index].title = title;
-            }
-        })
-
+        console.log(_this);
+        _this.lectureUpdates[id] = { id, title };
+        console.log("lecture Updates: ", _this.lectureUpdates);
     }
 
     updateSubtopicTitle([id, lectureID], title, _this){
-
-        let lectureIndex;
-
-        console.log("id:", id, "lectureID: ", lectureID)
-
-        _this.lectures.forEach( (lecture,index) => {
-            if(lecture.id == lectureID){
-                lectureIndex =  index;
-            }
-        })
-
-        _this.lectures[lectureIndex].subtopics.forEach( (subtopic,index) => {
-            if(subtopic.id == id){
-                _this.lectures[lectureIndex].subtopics[index].title = title;
-            }
-        })
-
-
-        console.log(_this.lectures)
-
+        _this.subtopicUpdates[id] = { id, lectureID, title };
+        console.log("subtopic Updates: ", _this.subtopicUpdates);
     }
 
-    
+    newLectureTitle(id, title, _this){
+        _this.newLectures[id] = { id, title };
+        console.log("new lecture: ", _this.newLectures);
+    }
+
+    deleteLectureTitle(id, title, _this){
+        _this.deleteLectures[id] = { id, title };
+        console.log("delete lecture: ", _this.deleteLectures);
+
+        // The function that will bind to the deleteLectures object
+        // will need to cascade by SQL to delete all subtopics
+        // that are bound to this lecture
+
+        // It will also need to erase all files and resources as well.
+    }
+
+    deleteSubtopicTitle([id, lectureID], title, _this){
+        _this.deleteSubtopic[id] = { id, title };
+        console.log("delete subtopic: ", _this.deleteSubtopic);
+    }
 
     renderLectureSection(){
-
-        let finalHTML = "";
 
         let courseGridContainer = document.querySelector("#course-grid-container");
 
@@ -93,6 +122,7 @@ class Course {
             lecture.id
             lecture.title
             lecture.subtopics
+            this.lectureIndex += 1;
 
             let lectureSection = document.createElement("div");
             lectureSection.className = "lecture-section";
@@ -104,7 +134,7 @@ class Course {
             let lectureInnerContainer = document.createElement("div");
             lectureInnerContainer.className = "lecture-inner-container";
 
-            let lectureInputElement = this.createInputElement(lecture.id, lecture.title, this.updateLectureTitle);
+            let lectureInputElement = this.createInputElement(lecture.id, lecture.title, this.updateLectureTitle, "lecture");
 
             lectureInnerContainer.appendChild(lectureInputElement);
 
@@ -119,7 +149,7 @@ class Course {
                 attachButton.innerHTML = `<img src="../assets/icons/fi/fi-rr-paperclip-vertical.svg" alt="">`;
                 attachButton.addEventListener("click", () => {});
 
-                let subtopicInputElement = this.createInputElement([subtopic.id, lecture.id], subtopic.title, this.updateSubtopicTitle);
+                let subtopicInputElement = this.createInputElement([subtopic.id, lecture.id], subtopic.title, this.updateSubtopicTitle, "subtopic");
                 subtopicInputElement.appendChild(attachButton);
 
                 subtopicsContainer.appendChild(subtopicInputElement);
@@ -130,13 +160,45 @@ class Course {
             lectureSection.appendChild(lectureInnerContainer);
             courseGridContainer.appendChild(lectureSection);
 
-
-            
         });
 
     }
 
-    createInputElement(id, title, inputChangeCallback){         
+    searchAndDeleteLecture(id){
+
+        this.lectures.forEach( (lecture, index) => {
+            if(lecture.id == id) {
+                this.deleteLectureTitle(id, "", this);
+                return;
+            }
+        })
+
+    }
+
+    searchAndDeleteSubtopic(id){
+
+        let lectureIndex = null;
+        let [_id, _lectureID ] = id;
+
+        this.lectures.forEach( (lecture,index) => {
+            if(lecture.id == _lectureID){
+                lectureIndex = index;
+                return;
+            }
+        })
+
+        if(lectureIndex != null){
+            this.lectures[lectureIndex].subtopics.forEach( (subtopic,index) => {
+                if(subtopic.id == _id){
+                    this.deleteSubtopicTitle(id, "", this);
+                    return;
+                }
+            })
+        }
+
+    }
+
+    createInputElement(id, title, inputChangeCallback, type){         
         let inputElementContainer = document.createElement("div");
         inputElementContainer.className = "input-element";
 
@@ -152,7 +214,23 @@ class Course {
 
         let deleteButton = document.createElement("div");
         deleteButton.className = "delete-button";
-        deleteButton.innerHTML = `<img src="../assets/icons/fi/fi-rr-delete.svg" alt="">`
+        deleteButton.innerHTML = `<img src="../assets/icons/fi/fi-rr-delete.svg" alt="">`;
+
+        deleteButton.addEventListener("click", () => {
+
+            switch(type){
+                case "lecture":
+                    inputElementContainer.parentElement.parentElement.remove();
+                    this.searchAndDeleteLecture(id)
+                    this.lectureIndex--
+                    break;
+                case "subtopic":
+                    inputElementContainer.remove();
+                    this.searchAndDeleteSubtopic(id)
+                    break;
+            }
+
+        });
 
         inputElementContainer.appendChild(inputElement);
         inputElementContainer.appendChild(deleteButton);
@@ -161,10 +239,6 @@ class Course {
     }
 
     renderSubtopics(lectureID){
-
-    }
-
-    displayCourseOutline(){
 
     }
 
@@ -191,6 +265,10 @@ let course = new Course(myCourse);
 course.renderTitle();
 course.renderCourseCode();
 course.renderLectureSection();
+
+document.querySelector("#addNewLecture").addEventListener("click", () => {
+    course.addLectureElement();
+})
 
 // TODO:  create a function that fetches all the data and
 // compiles it into the object we need
