18bbbe0947cb178e2278276a43bedfda961baa35
Ibrahim Ame <ai.iot@neu.edu.tr>
2024-03-07 19:07:28 +0200

    Trial Folder For PHP Testing

---
+++ include/trial/getCourseDetails.php
@@ -0,0 +1,108 @@
+<?php
+
+    include "../databaseConnection.php"; 
+
+    $conn = OpenConnection();
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $courseID = "ltcvladl";
+
+    if($courseID){
+
+        $query = "
+        SELECT *
+        FROM `courses` WHERE id = '$courseID'
+        ";
+
+        $result = mysqli_query($conn,$query);
+        $courses = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+        $finalResult = array();
+
+        foreach($courses as $course) {
+
+            $courseID = $course['id'];
+            
+            $lectureQuery = "
+            SELECT *
+            FROM `lectures` WHERE courseID = '$courseID'
+            ";
+
+            $lectureResult = mysqli_query($conn,$lectureQuery);
+            $lectures = mysqli_fetch_all($lectureResult,MYSQLI_ASSOC);
+
+            $lectureArray = array();
+
+            foreach($lectures as $lecture){
+
+                $lectureID = $lecture['id'];
+                
+                $subtopicQuery = "
+                SELECT *
+                FROM `subtopics` WHERE lectureID = '$lectureID'
+                ";
+
+                $subtopicResult = mysqli_query($conn,$subtopicQuery);
+                $subtopics = mysqli_fetch_all($subtopicResult,MYSQLI_ASSOC);
+
+                $subtopicArray = array();
+
+                foreach($subtopics as $subtopic){
+
+                    $subtopicID = $subtopic['id'];
+                    
+                    $resourceQuery = "
+                    SELECT *
+                    FROM `resources` WHERE subtopicID = '$subtopicID'
+                    ";
+    
+                    $resourcesResult = mysqli_query($conn,$resourceQuery);
+                    $resources = mysqli_fetch_all($resourcesResult,MYSQLI_ASSOC);
+
+                    $subtopicArray[] = array(
+                        "id" => $subtopicID,
+                        "title" => $subtopic['title'],
+                        "hierarchy" => $subtopic['hierarchy'],
+                        "resources" => $resources
+                    );
+                    
+                }
+
+                $quizQuery = "
+                SELECT *
+                FROM `quiz` WHERE lectureID = '$lectureID'
+                ";
+
+                $quizResult = mysqli_query($conn,$quizQuery);
+                $quizzes = mysqli_fetch_all($quizResult,MYSQLI_ASSOC);
+
+                $lectureArray[] = array(
+                    "id" => $lectureID,
+                    "title" => $lecture['title'],
+                    "hierarchy" => $lecture['hierarchy'],
+                    "subtopics" => $subtopicArray,
+                    "quizzes" => $quizzes
+                );
+
+            }
+
+            $resultA = array(
+                "id" => $course['id'],
+                "title" => $course['title'],
+                "courseCode" => $course['courseCode'],
+                "lectures" => $lectureArray
+            );
+
+            $finalResult[] = $resultA;
+
+        }
+
+        echo json_encode($finalResult);
+
+    }
+    else{
+        echo json_encode(array("status" => "error"));
+    }
