652a55369ffe0d7cdc971b6e8195027fc3f7451f
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-09-17 15:22:37 +0300

    Database Changes

--- include/databaseConnection.php
+++ include/databaseConnection.php
@@ -4,7 +4,7 @@
       $host = "localhost";
       $username = "aiiovdft_lms";
       $password = "strong#dux13";
-      $dbname = "aiiovdft_lms";
+      $dbname = "aiiovdft_profdux";
       
       $conn = new mysqli($host, $username, $password, $dbname) or die("Connection to database failed: %s\n". $conn -> error);
 
